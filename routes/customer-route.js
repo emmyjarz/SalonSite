d25826a7a053df4462eb534daaf086f8fbdeeeca
emmyjarz <emmyjarz@gmail.com>
2017-08-16 18:24:31 -0700

    folder structure

--- routes/customer-route.js
+++
@@ -1,9 +0,0 @@
-var db = require("../models");
-module.exports = function(app){
-    app.get("/", function(req,res){
-       
-        db.Customer.findAll({}).then(
-            res.render("index", {data: data})
-        )
-    })
-}
