13d665484a564a11432ea5af338a5f94dcee0204
emmyjarz <emmyjarz@gmail.com>
2017-08-19 18:46:11 -0700

    query salon info to about page

--- server.js
+++ server.js
@@ -47,8 +47,10 @@ app.use("/admin", admin);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+//{force:true}
