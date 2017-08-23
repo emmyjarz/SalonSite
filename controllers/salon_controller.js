87a6ca57fa1b9213a8a6a41a30cb3a2c80f21094
Meredith H <meredithhazan@gmail.com>
2017-08-23 14:48:01 -0700

    twilio post route

--- controllers/salon_controller.js
+++ controllers/salon_controller.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+var Twilio = require('twilio');
+var config = require('../config');
+var client = new Twilio(config.accountSid, config.authToken);
+
 
 //display
 router.get("/about", (req, res) => {
@@ -38,7 +42,7 @@ router.get("/contactus", (req, res) => {
     ]
   }).then(function (data) {
     // console.log(data);
-    console.log(data.dataValues.name);
+    /*console.log(data.dataValues.name);
     console.log(data.dataValues.description);
     console.log(data.Address.dataValues.address1);
     console.log(data.Address.dataValues.address2);
@@ -46,7 +50,7 @@ router.get("/contactus", (req, res) => {
     console.log(data.Address.dataValues.state);
     console.log(data.Address.dataValues.zip);
     console.log(data.Phone.dataValues.mobile);
-    console.log(data.Email.dataValues.email);
+    console.log(data.Email.dataValues.email);*/
     res.render("contactus", { contactus: data });
 
     });
@@ -55,6 +59,30 @@ router.get("/contactus", (req, res) => {
 router.get("/", (req, res) => {
   res.render("index", { data: hello });
 });
+
+router.post("/leads", (req, res) => {
+  var textString = 
+`You have a new lead -
+Name: ${req.body.firstNameContact}
+Phone: ${req.body.phoneContact}
+Email: ${req.body.emailContact}
+Reason for contact: ${reasonContact}
+Additional comments: ${addlContact}`
+
+client.messages.create({
+    to: 'config.salonNumber',  // Text this number
+    from: 'config.twilioNumber' // From a valid Twilio number
+    body: textString,
+})
+.then(() => {
+          // Otherwise, respond with 200 OK
+          response.status(200).send('Lead notification was successfully sent.');
+        })
+        .catch((err) => {
+          console.error(err);
+          response.status(500).send();
+        })
+    });
 
 router.get("/products", (req, res) => {
 
---
+++ public/assets/js/config.js
@@ -0,0 +1,7 @@
+var accountSid = 'ACbee7cda09d1d49514c2b47f0a2bbc5f2'; // Your Account SID from www.twilio.com/console
+var authToken = '9a5145f6e795c1911336d504e93eae4f';   // Your Auth Token from www.twilio.com/console
+
+var salonNumber = '+13236202056';
+var twilioNumber = '+18182147591';
+
+module.exports = config;
