215831c56a22491cf7fbb3ee781cf235ebfd60e8
emmyjarz <emmyjarz@gmail.com>
2017-08-23 00:26:29 -0700

    add admin product service handlebars

--- controllers/salon_controller.js
+++ controllers/salon_controller.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
-
+const SALON_NAME = "Blvd6 Salon";
 //display
 router.get("/about", (req, res) => {
   //show salon info on the about page
   db.Salon.findOne({
     where: {
-      name: "Blvd6 Salon"
+      name: SALON_NAME
     },
     include: [db.Address
       , db.Email, db.Phone
@@ -32,12 +32,12 @@ router.get("/about", (req, res) => {
 router.get("/contactus", (req, res) => {
   db.Salon.findOne({
     where: {
-      name: "Blvd6 Salon"
+      name: SALON_NAME
     },
     include: [db.Address
       , db.Email, db.Phone
     ]
-  }).then(function (data) {
+  }).then( data=> {
     // console.log(data);
     // console.log(data.dataValues.name);
     // console.log(data.dataValues.description);
@@ -52,21 +52,54 @@ router.get("/contactus", (req, res) => {
 
     });
 });
-//show product on the product page
-router.get("/", (req, res) => {
-  res.render("index", { data: hello });
-});
 
+//show product brand on the product page
 router.get("/products", (req, res) => {
-  res.render("products", { data: "hello" });
+  db.Product.findAll({
+    attributes:["brand"],
+    group:"brand"
+  }).then(data =>{
+    // console.log(data);
+    
+    //  console.log(data[0].dataValues.brand);
+    res.render("products", { productBrands: data });
+  })
 });
 
+// show products in the brand
+router.get("/products/:brand", (req, res)=>{
+  db.Product.findAll({
+    where:{
+      brand:req.params.brand
+    } 
+  }).then(data =>{
+    // console.log(data)
+    res.render("products", {products: data});
+  });
+});
+//show staff
+router.get("/staff", (req, res)=>{
+  db.Staff.findAll({
+    include: [db.Address
+      , db.Email, db.Phone
+    ]
+  }).then(data=>{
+    console.log(data);
+    res.render("staff", {staff:data});
+  })
+})
+
+router.get("/", (req, res) => {
+  res.render("index", { data: "hello" });
+});
+//show services
 router.get("/services", (req, res) => {
   db.Service.findAll().then(data => {
-    console.log(data);
+    // console.log(data);
     res.render("services", { services: data });
   })
 });
+
 
 
 
