f4e59fadefd001abe0004eec275f9723b63f6754
Meredith H <meredithhazan@gmail.com>
2017-08-23 18:34:13 -0700

    product updates

--- supplement/database/productinfoseed.sql
+++ supplement/database/productinfoseed.sql
@@ -4,7 +4,7 @@ INSERT INTO products (brand, name, description, size, price, stock_quantity, cos
 
 INSERT INTO products (brand, name, description, size, price, stock_quantity, cost, vendor, photo, createdAt, updatedAt) VALUES("Eleven", "shampoo", "very good", "20ml", 10.99, 10, 5.99, "test3", now(), now());
 
-INSERT INTO products (brand, name, description, size, price, stock_quantity, cost, vendor, photo, createdAt, updatedAt) VALUES("Zenagen", "Revolve Treatment for Women", "Zenagen Revolve Treatment for Women is a non daily, luxurious, naturally derived hair loss treatment system specifically tailored for female hair loss, thinning, and shedding. 1 bottle of the shampoo based Zenagen Revolve Treatment for Women will last 3 months, and results can start to be seen as early as 4 weeks after using the product consistently.", "20ml", 10.99, 10, 5.99, "test4", now(), now());
+INSERT INTO products (brand, name, description, size, price, stock_quantity, cost, vendor, photo, createdAt, updatedAt) VALUES("Zenagen", "Revolve Treatment for Women", "Zenagen Revolve Treatment for Women is a non daily, luxurious, naturally derived hair loss treatment system specifically tailored for female hair loss, thinning, and shedding. 1 bottle of the shampoo based Zenagen Revolve Treatment for Women will last 3 months, and results can start to be seen as early as 4 weeks after using the product consistently.", "20ml", 10.99, 10, 5.99, "../../public/assets/images/products/Zen_Revolve198x300.png", now(), now());
 
 
 SELECT * FROM products;
