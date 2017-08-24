91a389202a58a652fe3614a74f63037a64ee7731
Meredith H <meredithhazan@gmail.com>
2017-08-23 23:36:07 -0700

    removing config.js

--- config/js/config.js
+++
@@ -1,8 +0,0 @@
-var Config = {
-accountSid: 'ACbee7cda09d1d49514c2b47f0a2bbc5f2', // Your Account SID from www.twilio.com/console
-authToken: '9a5145f6e795c1911336d504e93eae4f',   // Your Auth Token from www.twilio.com/console
-salonNumber: '+13236202056',
-twilioNumber: '+18182147591'
-};
-
-module.exports = Config;
