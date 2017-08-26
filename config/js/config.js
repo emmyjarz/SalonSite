cf870c78bd406c4cdc832abdd83d235e3dfdeabd
sherman_mortek <shermangore@gmail.com>
2017-08-26 08:42:30 -0700

    Re-added accidentally removed appointment button

--- config/js/config.js
+++ config/js/config.js
@@ -1,8 +1,12 @@
 var Config = {
-    accountSid: process.env.twilio_Sid, // Your Account SID from www.twilio.com/console
-    authToken: process.env.twilio_Token,   // Your Auth Token from www.twilio.com/console
-    salonNumber: '+' + process.env.twilio_Phone_1,
-    twilioNumber: '+' + process.env.twilio_Phone_2
+    // accountSid: process.env.twilio_Sid, // Your Account SID from www.twilio.com/console
+    // authToken: process.env.twilio_Token,   // Your Auth Token from www.twilio.com/console
+    // salonNumber: '+' + process.env.twilio_Phone_1,
+    // twilioNumber: '+' + process.env.twilio_Phone_2
+    accountSid: 'ACbee7cda09d1d49514c2b47f0a2bbc5f2', // Your Account SID from www.twilio.com/console
+    authToken: '9a5145f6e795c1911336d504e93eae4f',   // Your Auth Token from www.twilio.com/console
+    salonNumber: '+13236202056',
+    twilioNumber: '+18182147591'
 };
     
 module.exports = Config;
